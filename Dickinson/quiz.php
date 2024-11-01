296474892c03ca75a15afc00c06f157802963963
Isabelle Ruivo <isabelleruivosantos@gmail.com>
2024-11-01 17:46:27 -0300

    Add files via upload

---
+++ Dickinson/quiz.php
@@ -0,0 +1,324 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+	<head>
+		<meta charset="UTF-8">
+		<title>Quiz de Dickinson</title>
+		<meta name="author" content="Isabelle Ruivo">
+		<meta name="description" content="Série Dickinson">
+		<meta name="keywords" content="Dickinson, Emily, Amherst, Sue, Austin, Poemas, Poesia, Apple TV">
+		<link rel="icon" href="../Dickinson/images/icon_appletv.png">
+	</head>
+	<body background="images/background_azul_escuro.jpg">
+			
+		<font face="Garamond" size="4" color="#cfeef9" align="center">
+				<h1>E para os mais chegados, faça um quiz para testar seu conhecimento a respeito da série e de Emily Dickinson!</h1>	
+		</font>
+
+		<audio controls loop>
+			<source src="audio/AfterlifeDickinson.mp3" type="audio/mp3">
+		</audio>
+
+		<font face="Garamond" size="4" color="#cfeef9">
+			<p align="center">
+				<a href="#Resultado"><font face="Garamond" size="5">Clique aqui APÓS enviar suas respostas para ver seu resultado!</a>
+		</font>
+		
+		<form name="quiz" action="#" method="get">
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 1</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Em que período a série Dickinson é retratada?
+							</font>
+							<br>
+								<input type="radio" name="questao1" value="0">
+									<font face="Garamond" size="4" color="#cfeef9">
+							a) Século 20, em 1902&nbsp;&nbsp;&nbsp;
+								<input type="radio" name="questao1" value="1">
+							b) Século 19, em 1800&nbsp;&nbsp;&nbsp;
+								<input type="radio" name="questao1" value="0">
+							c) Século 19, em 1850&nbsp;&nbsp;&nbsp;
+								<input type="radio" name="questao1" value="0">
+							d) Século 20, em 1910&nbsp;&nbsp;&nbsp;
+						</p>
+				</fieldset>
+				<br>
+			</font>
+
+			<font face="Garamond" size="5" color="#cfeef9">		
+				<fieldset>
+					<legend>Questão 2</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Onde Emily Dickinson morava?
+							</font>
+						<br>
+							<input type="radio" name="questao2" value="1">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Amherst, Massachusetts
+							<input type="radio" name="questao2" value="0">
+						b) Boston, Massachusetts
+							<input type="radio" name="questao2" value="0">
+						c) Hartford, Connecticut
+							<input type="radio" name="questao2" value="0">
+						d) Pennsylvania, Nova York
+						</p>
+				</fieldset>
+				<br>
+			</font>
+
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 3</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Desde o começo da série, há um rapaz que sempre perseguiu romanticamente a Emily, mesmo não sendo correspondido. Quem é esse rapaz?
+							</font>
+						<br>
+							<input type="radio" name="questao3" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Joseph Lyman 
+							<input type="radio" name="questao3" value="0">
+						b) Ben Newton
+							<input type="radio" name="questao3" value="1">
+						c) George Gould
+							<input type="radio" name="questao3" value="0">
+						d) Sam Bowles
+						</p>
+				</fieldset>
+				<br>
+			</font>
+					
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 4</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Emily queria muito ser publicada, então Sue apresenta um homem capaz de publicar seus poemas no jornal. Quem é esse homem?
+							</font>
+						<br>
+							<input type="radio" name="questao4" value="1">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Sam Bowles
+							<input type="radio" name="questao4" value="0">
+						b) Henry Shipley
+							<input type="radio" name="questao4" value="0">
+						c) Joseph Lyman 
+							<input type="radio" name="questao4" value="0">
+						d) Henry David Thoreau]
+						</p> 
+				</fieldset>
+				<br>
+			</font>
+
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 5</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								O final da 1ºTemporada deixou muitos surpresos, pois, uma mulher fica grávida. Quem é essa mulher?
+							</font>
+						<br>
+							<input type="radio" name="questao5" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Emily Dickinson
+							<input type="radio" name="questao5" value="1">
+						b) Sue Gilbert
+							<input type="radio" name="questao5" value="0">
+						c) Mãe da Emily
+							<input type="radio" name="questao5" value="0">
+						d) Lavinia Dickinson
+						</p>
+				</fieldset>
+				<br>
+			</font>
+					
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+						<legend>Questão 6</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Na 2ºTemporada, uma guerra está perto de acontecer, que guerra seria essa?
+							</font>
+						<br>
+							<input type="radio" name="questao6" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Guerra Fria
+							<input type="radio" name="questao6" value="1">
+						b) Guerra Civil Americana
+							<input type="radio" name="questao6" value="0">
+						c) Segunda Guerra Mundial
+							<input type="radio" name="questao6" value="0">
+						d) Guerra do Vietnã
+						</p>
+				</fieldset>
+				<br>
+			</font>
+					
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 7</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Emily pegava carona com alguém, numa carroagem. Quem é essa pessoa?
+							</font>
+						<br>
+							<input type="radio" name="questao7" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Seu irmão, Austin
+							<input type="radio" name="questao7" value="1">
+						b) A Morte
+							<input type="radio" name="questao7" value="0">
+						c) Thomas Wentworth Higginson
+							<input type="radio" name="questao7" value="0">
+						d)  Abby Wood
+						</p>
+				</fieldset>
+				<br>
+			</font>
+					
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 8</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Quantos poemas de Emily Dickinson foram descobertos após sua morte?
+							</font>
+						<br>
+							<input type="radio" name="questao8" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Cerca de 2800 poemas
+							<input type="radio" name="questao8" value="0">
+						b) Cerca de 2000 poemas
+							<input type="radio" name="questao8" value="0">
+						c) Cerca de 1300 poemas
+							<input type="radio" name="questao8" value="1">
+						d) Cerca de 1800 poemas
+						</p>
+				</fieldset>
+				<br>
+			</font>
+					
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 9</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Qual a causa da morte de Emily Dickinson?
+							</font>
+						<br>
+							<input type="radio" name="questao9" value="0">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a) Tuberculose 
+							<input type="radio" name="questao9" value="0">
+						b) Câncer de pâncreas
+							<input type="radio" name="questao9" value="1">
+						c) Nefrite
+							<input type="radio" name="questao9" value="0">
+						d) Pneumonia
+						</p>
+				</fieldset>
+				<br>
+			</font>
+
+			<font face="Garamond" size="5" color="#cfeef9">
+				<fieldset>
+					<legend>Questão 10</legend>
+						<p align="center">
+							<font face="Garamond" size="5" color="#cfeef9">
+								Emily era/é bastante denominada por alguns como?
+							</font>
+						<br>
+							<input type="radio" name="questao10" value="1">
+								<font face="Garamond" size="4" color="#cfeef9">
+						a)  Grande reclusa
+							<input type="radio" name="questao10" value="0">
+						b)	Extrovertida
+							<input type="radio" name="questao10" value="0">
+						c)	Ignorante
+							<input type="radio" name="questao10" value="0">
+						d)	Moça comportada
+						</p>
+				</fieldset>
+			</font>
+				<p align="center">
+					<input type="submit" name="Enviar" value="Enviar">
+				</p>
+				<br><br><br><br><br><br>
+		</form>
+
+		<?php
+
+			if(isset($_GET['Enviar'])) {
+
+				$q1=$_GET['questao1'];
+				$q2=$_GET['questao2'];
+				$q3=$_GET['questao3'];
+				$q4=$_GET['questao4'];
+				$q5=$_GET['questao5'];
+				$q6=$_GET['questao6'];
+				$q7=$_GET['questao7'];
+				$q8=$_GET['questao8'];
+				$q9=$_GET['questao9'];
+				$q10=$_GET['questao10'];
+				$acertos=$q1+$q2+$q3+$q4+$q5+$q6+$q7+$q8+$q9+$q10;
+
+				echo ('<p align="center"');
+				echo ('<font face="Garamond" size=6 color="white">');
+				
+				if ($acertos<4) {
+					echo ('<font face="Garamond" size=6 color="#cfeef9">');
+					echo ("Você acertou ".$acertos." questões. Poxa, vejo que não sabe quase nada sobre Emily Dickinson ou sobre a série...");
+					echo ('<figure><p align="center"><img src="images/quiz_I.gif" widht="510" height="270"></figure>');
+				} elseif ($acertos>3 && $acertos<7) {
+					echo ('<font face="Garamond" size=6 color="#cfeef9">');
+					echo ("Você acertou ".$acertos." questões. Você até que foi bem, mas pode ser melhor que isso, viu?!");
+					echo ('<figure><p align="center"><img src="images/quiz_R.gif" widht="555" height="282"></figure>');
+				} elseif ($acertos>6 && $acertos<10) {
+					echo ('<font face="Garamond" size=6 color="#cfeef9">');
+					echo ("Você acertou ".$acertos." questões. Uau! Você sabe quase tudo, parabéns!");
+					echo ('<figure><p align="center"><img src="images/quiz_B.gif" widht="520" height="280"></figure>');
+				} else {
+					echo ('<font face="Garamond" size=6 color="#cfeef9">');
+					echo ("Você acertou ".$acertos." questões. INACREDITÁVEL! Você é realmente um grande fã da obra, continue assim!");
+					echo ('<figure><p align="center"><img src="images/quiz_MB.gif"></figure>');
+				}
+			}
+		?>
+		<a name="Resultado"></a>
+		<hr size="1" width="95%" color="#f3cd42">
+
+		<tfoot>
+			<nav>
+				<article>
+					<table border="0" width="100%" height="50%" bordercolor="black" align="center">
+						<tbody>
+							<tr>
+								<td align="left" width="2200px" height="60px">
+									<a href="quiz.php" title="Quiz" target="home"><font color="#f3cd42" size="5" face="Garamond">Deseja refazer o quiz?</a>
+								</td>
+								<td align="right" width="2200px" height="60px">
+									<font face="Garamond" size="5" color="white">Contato:</font>
+								</td>
+								<td align="center" width="100px" height="60px">
+									<a href="https://wa.me/5511975813773" target="_blank">
+										<img src="images/whatsapp_logo.png" title="Whatsapp" width="80" height="50"></a></td>
+								<td align="center" width="100px" height="60px">
+									<a href="https://twitter.com/gothgirIclique_" target="_blank">
+										<img src="images/twitter_logo.png" title="Twitter" width="45" height="45"></a></td>
+								<td align="center" width="100px" height="60px">
+									<a href="https://www.instagram.com/iswwruivo_/" target="_blank">
+										<img src="images/instagram_logo.png" title="Instagram" width="45" height="45"></a></td>
+								<td align="center" width="100px" height="60px">
+									<a href="https://br.pinterest.com/belleyeiswan/_saved/" target="_blank">
+										<img src="images/pinterest_logo.png" title="Pinterest" width="65" height="65">
+									</a></td>
+							</tr>
+						</tbody>
+					</table>
+				</article>
+			</nav>
+		</tfoot>
+	</body>
+</html>
